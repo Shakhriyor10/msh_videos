8114eb80892733b6724fbe708d806d0f3bac84c0
Shakhriyor10 <nnnnafisa@gmail.com>
2026-02-06 15:36:40 +0500

    sjdksjdk

--- msh_videos/urls.py
+++ msh_videos/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from msh_videos import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("product.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Product
+
+
+def home(request):
+    products = (
+        Product.objects.select_related()
+        .prefetch_related("categories", "tags")
+        .all()
+    )
+    hot_products = products.filter(is_hot=True)[:4]
+    return render(
+        request,
+        "product/home.html",
+        {
+            "products": products,
+            "hot_products": hot_products,
+        },
+    )
+
+
+def product_detail(request, pk: int):
+    product = get_object_or_404(
+        Product.objects.prefetch_related("categories", "tags"),
+        pk=pk,
+    )
+    return render(
+        request,
+        "product/detail.html",
+        {"product": product},
+    )
